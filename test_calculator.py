241cb59a12db2b2d44901fb5b5d509c7eb1198bd
Juha Loukaja <loukaja@gmail.com>
2024-11-29 08:22:28 +0200

    Add test for multiplication and division

--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,9 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 25 == calculator.multiply(5, 5)
+
+    def tets_division(self):
+        assert 2 == calculator.divide(18, 9)
